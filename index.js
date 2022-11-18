5e0d6f995f0904414b8864a947fa1e723082adfa
JahDM <hubertwatycha20013112@gmail.com>
2022-11-18 14:30:19 +0100

    conditions

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-
-
 function getComputerChoice(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const item = arr[randomIndex];
-    return item;   
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const item = arr[randomIndex];
+  return item;
 }
-const array = ['Rock', 'Paper', 'Scissors']
+let array = ["Rock", "Paper", "Scissors"];
+let result = getComputerChoice(array);
 
-const result = getComputerChoice(array);
 console.log(result);
+const computerSelection = result;
+let userInput = prompt("Rock, Paper, Scissors");
+console.log(userInput);
+const playerSelection = userInput.toLocaleLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw";
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      return "You win";
+    } else if (computerSelection === "Scissors") {
+      return "You lose";
+    } else return "Draw";
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      return "You win";
+    } else if (computerSelection === "Paper") {
+      return "You lose";
+    } else return "Draw";
+  } else {
+    if (computerSelection === "Paper") {
+      return "You win";
+    } else if (computerSelection === "Rock") {
+      return "You lose";
+    } else return "Draw";
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
