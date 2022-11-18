84b40b8bee523b2fdb6fa1dbb7b7f25a35818cb7
JahDM <hubertwatycha20013112@gmail.com>
2022-11-18 20:44:48 +0100

    conditions

--- index.js
+++ index.js
@@ -6,34 +6,41 @@ function getComputerChoice(arr) {
 let array = ["Rock", "Paper", "Scissors"];
 let result = getComputerChoice(array);
 
-console.log(result);
-const computerSelection = result;
-let userInput = prompt("Rock, Paper, Scissors");
-console.log(userInput);
-const playerSelection = userInput.toLocaleLowerCase();
+//console.log(result);
+
+const userInput = prompt("Rock, Paper, Scissors");
+//console.log(userInput);
+
+const playerSelection = userInput.toLowerCase();
+const computerSelection = result.toLowerCase();
+//console.log(computerSelection);
+//console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "Draw";
-  } else if (playerSelection === "Paper") {
-    if (computerSelection === "Rock") {
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
       return "You win";
-    } else if (computerSelection === "Scissors") {
+    } else if (computerSelection === "scissors") {
       return "You lose";
     } else return "Draw";
-  } else if (playerSelection === "Rock") {
-    if (computerSelection === "Scissors") {
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
       return "You win";
-    } else if (computerSelection === "Paper") {
+    } else if (computerSelection === "paper") {
       return "You lose";
     } else return "Draw";
-  } else {
-    if (computerSelection === "Paper") {
+  } else (playerSelection === "scissors"); {
+    if (computerSelection === "paper") {
       return "You win";
-    } else if (computerSelection === "Rock") {
+    } else if (computerSelection === "rock") {
       return "You lose";
     } else return "Draw";
   }
 }
-
 console.log(playRound(playerSelection, computerSelection));
+
+
+  
+
